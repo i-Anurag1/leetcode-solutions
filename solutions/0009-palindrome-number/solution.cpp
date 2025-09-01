c59f1f74efea575fab7dfba8de837c3c45676773
anurag_thakur <anurag27108@gmail.com>
2025-09-01 21:00:43 +0000

    [LeetCode Sync] Runtime - 1 ms (57.63%), Memory - 8.6 MB (65.21%)

--- solutions/0009-palindrome-number/solution.cpp
+++ solutions/0009-palindrome-number/solution.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int n=0,k,o=x;
-        if(x<0){
-            return false;
-        }
-        while(x!=0){
-            if(n>(INT_MAX- o%10)/10){
-                return false;
-            }
-             k=x%10;
-            n=n*10+k;
-            x=x/10;
-        }
-        if(n==o){
-            return true;
-        }
-        else{
-            return false;
-        }
-        
+        long long ans=0, n=x;
+        if(n<0)
+        return 0;
+      while(n)
+      {
+        int r=n%10;
+        n/=10;
+        ans=ans*10+r;
+        if(ans>INT_MAX || ans<INT_MIN ){
+        return 0;}
+      } 
+      if(ans==x) 
+      return 1;
+      else 
+      return 0;
     }
 };
