b00c07b20c90252e84a3674b3f61cbf878333f23
anurag_thakur <anurag27108@gmail.com>
2026-03-18 06:32:12 +0000

    [LeetCode Sync] Runtime - 23 ms (55.20%), Memory - 17.4 MB (53.21%)

---
+++ solutions/0018-4sum/solution.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+
+        if(n < 4) return ans;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < n - 3; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for(int j = i + 1; j < n - 2; j++){
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while(left < right){
+                    long long sum = 0;
+                    sum += nums[i];
+                    sum += nums[j];
+                    sum += nums[left];
+                    sum += nums[right];
+
+                    if(sum == target){
+                        vector<int> temp;
+                        temp.push_back(nums[i]);
+                        temp.push_back(nums[j]);
+                        temp.push_back(nums[left]);
+                        temp.push_back(nums[right]);
+                        ans.push_back(temp);
+
+                        left++;
+                        right--;
+
+                        while(left < right && nums[left] == nums[left - 1]) left++;
+                        while(left < right && nums[right] == nums[right + 1]) right--;
+                    }
+                    else if(sum < target){
+                        left++;
+                    }
+                    else{
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
