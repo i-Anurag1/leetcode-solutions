9825792c2e0fb9d421ce362814e8c37f66b448b3
anurag_thakur <anurag27108@gmail.com>
2026-03-18 06:49:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.1 MB (89.21%)

---
+++ solutions/0728-self-dividing-numbers/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> ans;
+
+        for(int i = left; i <= right; i++){
+            int n = i;
+            bool ok = true;
+
+            while(n){
+                int d = n % 10;
+
+                if(d == 0 || i % d != 0){
+                    ok = false;
+                    break;
+                }
+
+                n = n / 10;
+            }
+
+            if(ok) ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
