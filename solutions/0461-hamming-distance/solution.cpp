d9d6d59316936939368235234ae87e43cf7d118f
anurag_thakur <anurag27108@gmail.com>
2026-03-22 08:12:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8 MB (37.38%)

--- solutions/0461-hamming-distance/solution.cpp
+++ solutions/0461-hamming-distance/solution.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
-        int val = x ^ y;
-        
-        int count = 0;
-        
-        while (val > 0) {
-            val = val & (val - 1);
-            count++;
-        }
-        
-        return count;
+        return __builtin_popcount(x ^ y);
     }
 };
