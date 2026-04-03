919793a67fada0e041712e1a722a46562e65c035
anurag_thakur <anurag27108@gmail.com>
2026-04-03 12:30:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.8 MB (61.36%)

---
+++ solutions/1960-check-if-the-sentence-is-pangram/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        vector<bool>check(26,0);
+        for(int i=0;i<sentence.size();i++){
+            check[sentence[i]-'a']=1;
+        }
+                for(int i=0;i<26;i++){
+                 if(check[i]==0) return false;
+                 
+                }
+return true;
+    }
+};
