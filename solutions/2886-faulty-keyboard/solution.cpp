360cd0c88d92c6e66216b1172c852f74c4315c79
anurag_thakur <anurag27108@gmail.com>
2026-04-10 19:30:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.3 MB (63.44%)

---
+++ solutions/2886-faulty-keyboard/solution.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string finalString(string s) {
+        string res = "";
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'i')
+                reverse(res.begin(), res.end());
+            else
+                res += s[i];
+        }
+
+        return res;
+    }
+};
