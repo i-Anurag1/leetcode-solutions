dc3442d670041916636c5fe9495f81d6ae6c2961
anurag_thakur <anurag27108@gmail.com>
2026-04-20 10:01:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 28.1 MB (63.62%)

--- solutions/0169-majority-element/solution.cpp
+++ solutions/0169-majority-element/solution.cpp
@@ -1,21 +1,22 @@
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int cand, count=0;
-        for(int i=0;i<nums.size();i++){
-            if(count ==0){
-                count++;
-                cand=nums[i];
+        int cnt = 0;
+        int cand = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(cnt == 0){
+                cand = nums[i];
             }
-            else{
-                if(cand==nums[i]){
-                    count++;
-                }
-                else{
-                    count--;
-                }
+
+            if(nums[i] == cand){
+                cnt++;
+            } else {
+                cnt--;
             }
         }
+
         return cand;
     }
 };
